7c6f5acee0e7f7d5347f09cbfef38ca7a713f614
rahultumpala <rhltumpala@gmail.com>
2023-07-11 10:48:28 +0530

    VM - end - Binary Operators

--- src/vm.c
+++ src/vm.c
@@ -32,6 +32,7 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) do {double b = pop(); double a = pop(); push(a op b);} while(false)
 
     for(;;){
         uint8_t instruction;
@@ -52,6 +53,10 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
             case OP_NEGATE:
                 push(-pop());
                 break;
@@ -67,6 +72,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
