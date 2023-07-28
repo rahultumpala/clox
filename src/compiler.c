5bb851e9e346ab7910a9a95480d97831a309c2fb
rahultumpala <rhltumpala@gmail.com>
2023-07-28 17:53:07 +0530

    if, else, while, for - end

--- src/compiler.c
+++ src/compiler.c
@@ -500,6 +500,7 @@ static void expressionStatement() {
 }
 
 static void forStatement() {
+    beginScope();
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
     if (match(TOKEN_SEMICOLON)){
         // No initializer.
@@ -510,11 +511,38 @@ static void forStatement() {
     }
 
     int loopStart = currentChunk()->count;
+    int exitJump = -1;
+    if(!match(TOKEN_SEMICOLON)){
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
     consume(TOKEN_SEMICOLON, "Expect ';'.");
-    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+    // increment clause
+    if(!match(TOKEN_RIGHT_PAREN)){
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
 
     statement();
     emitLoop(loopStart);
+
+    if(exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
 }
 
 static void ifStatement() {
