e4a5d31b6d80ef069b271248eccc39551530acd1
rahultumpala <rhltumpala@gmail.com>
2023-08-03 18:33:00 +0530

    Calls and functions - return statements

--- src/compiler.c
+++ src/compiler.c
@@ -648,6 +648,20 @@ static void printStatement() {
     emitByte(OP_PRINT);
 }
 
+static void returnStatement() {
+    if(current->type == TYPE_SCRIPT) {
+        error("Can't return from top-level code.");
+    }
+
+    if(match(TOKEN_SEMICOLON)) {
+        emitReturn();
+    } else {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+        emitByte(OP_RETURN);
+    }
+}
+
 static void whileStatement() {
     int loopStart = currentChunk()->count;
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
@@ -714,6 +728,8 @@ static void statement() {
         whileStatement();
     } else if (match(TOKEN_FOR)) {
         forStatement();
+    } else if (TOKEN_RETURN) {
+        returnStatement();
     } else {
         expressionStatement();
     }
